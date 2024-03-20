be01b93e53ec3bbefc591dccb17005aeaa9f6561
platon-reshetnikov <ctrather2@gmail.com>
2024-03-20 17:29:21 +0200

    Second commit

---
+++ Server/AuthenticationManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class AuthenticationManager
+    {
+        private List<User> users;
+
+        public AuthenticationManager()
+        {
+       
+            users = new List<User>();
+        }
+
+        
+        public void AddUser(User user)
+        {
+            users.Add(user);
+        }
+
+        
+        public User Authenticate(string username, string password)
+        {
+            foreach (User user in users)
+            {
+                if (user.Username == username && user.Password == password)
+                {
+                    return user; 
+                }
+            }
+            return null; 
+        }
+    }
+}
---
+++ Server/UserManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class UserManager
+    {
+        private List<User> users;
+
+        public UserManager()
+        {
+            
+            users = new List<User>();
+        }
+
+        
+        public void AddUser(User user)
+        {
+            users.Add(user);
+        }
+
+        
+        public void DeleteUser(string username)
+        {
+            
+            User userToRemove = users.Find(u => u.Username == username);
+            if (userToRemove != null)
+            {
+                users.Remove(userToRemove);
+            }
+        }
+
+        
+        public void UpdateUserRole(string username, string newRole)
+        {
+            
+            User userToUpdate = users.Find(u => u.Username == username);
+            if (userToUpdate != null)
+            {
+                userToUpdate.Role = newRole;
+            }
+        }
+    }
+}
