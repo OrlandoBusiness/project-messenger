91d2c56dc22eb3eee2d55fc20ea80f3407a5edee
Ярослав Ровнянский <jarizleifr06@gmail.com>
2024-03-25 17:29:48 +0100

    ViewModel Bones

---
+++ Client/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfMvvmPhonebook_P1415.Infrastructure;
+using System.ComponentModel;
+
+namespace Client.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        TcpClient _tcpClient;
+        NetworkStream _stream;
+
+        Timer _timer;
+        private ICommand? sendButton;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public ICommand? SendButton => sendButton ??= new RelayCommand(
+            param =>
+            {
+
+               
+
+            });
+        public MainWindowViewModel() 
+        {
+            
+            //_tcpClient = tcpClient;
+            _stream = _tcpClient.GetStream();
+        }
+    }
+}
