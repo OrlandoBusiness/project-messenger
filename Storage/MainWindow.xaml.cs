170960ab45e6ca5bfd99e89b041ec498a60827a9
Ярослав Ровнянский <jarizleifr06@gmail.com>
2024-03-26 21:51:30 +0100

    Finished Storage

--- Storage/MainWindow.xaml.cs
+++ Storage/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Model;
+using Storage.ViewModel;
 using System.IO;
 using System.Text;
 using System.Windows;
@@ -25,63 +26,11 @@ namespace Storage
         public MainWindow()
         {
             InitializeComponent();
-            try
-            {
-                string connectionString = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build().GetConnectionString("SqlClient");
-
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-                    var sql = @"SELECT p.Id, p.Name,p.Price,p.LifeInDays, m.Id, m.Name, s.Password , s.Id, s.Name
-                                FROM [Product] p 
-                                INNER JOIN Measure m ON p.MeasureId = m.Id
-                                INNER JOIN ProductSupplier ps ON p.Id = ps.ProductId
-                                INNER JOIN Supplier s ON ps.SupplierId = s.Id";
-                    IEnumerable<Product> resultProduct = connection.Query<Product, Measure, Supplier, Product >(sql, (product, mesure,suplier) => 
-                    {
-                        product.Measure = mesure;
-                        product.Suppliers.Add(suplier);
-                        return product;
-                    });
-
-                    foreach (Product value in resultProduct)
-                    {
-                        products.Add(value);
-                        ProductNames.Items.Add(value.Name);
-                    }
-                    sql = @"SELECT o.Id, o.Count, p.Name
-                            FROM [Order] o
-                            INNER JOIN Product p ON p.Id = o.ProductId";
-                    IEnumerable<Order> resultOrder = connection.Query<Order, Product, Order>(sql, (order, product) =>
-                    {
-                        order.Product = product;
-                        return order;
-                    },splitOn:"Name");
-                    foreach (Order value in resultOrder) 
-                    {
-                        ActiveListBox.Items.Add($"{value.Id},{value.Product.Name},{value.Count}");
-                    }
-                    sql = @"SELECT s.Id, s.Count,s.Price,s.ShippingDate, p.Name
-                            FROM [Supplie] s
-                            INNER JOIN Product p ON p.Id = s.ProductId";
-                    IEnumerable<Supplies> resultSupplies = connection.Query<Supplies, Product, Supplies>(sql, (supplies, product) =>
-                    {
-                        supplies.Product = product;
-                        return supplies;
-                    }, splitOn: "Name");
-                    foreach (Supplies value in resultSupplies)
-                    {
-                        PickedListBox.Items.Add($"{value.Id},{value.Product.Name},{value.Count},{value.Price},{value.ShippingDate.ToShortDateString()}");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+            DataContext = new MainWindowViewModel(this);
+            
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        /*
+        private void Button_Click(object sender, RoutedEventArgs e) Click="Button_Click"
         {
             try
             {
@@ -91,6 +40,20 @@ namespace Storage
                     connection.Query<Order>("INSERT [Order] VALUES (@ProductId,@StorageId, @Count,1)",
                 new { ProductId = products.Find(p => p.Name == ProductNames.SelectedItem).Id, StorageId = 1, Count = IntegerCount.Value });
 
+
+                    var sql = @"SELECT o.Id, o.Count, p.Name
+                            FROM [Order] o
+                            INNER JOIN Product p ON p.Id = o.ProductId";
+                    IEnumerable<Order> resultOrder = connection.Query<Order, Product, Order>(sql, (order, product) =>
+                    {
+                        order.Product = product;
+                        return order;
+                    }, splitOn: "Name");
+                    ActiveListBox.Items.Clear();
+                foreach (Order value in resultOrder)
+                {
+                    ActiveListBox.Items.Add($"{value.Id},{value.Product.Name},{value.Count}");
+                }
                 }
             }
             catch(Exception ex) 
@@ -102,8 +65,8 @@ namespace Storage
         private void ProductNames_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             SendButton.IsEnabled = true;
-            var obj = products.Find(p => p.Name == ProductNames.SelectedItem);
+            var obj = products.Find(p => p.Name == ProductNames.SelectedItem); SelectionChanged = "ProductNames_SelectionChanged"
             MeasureLabel.Content = obj.Measure.Name;
-        }
+        }*/
     }
 }
---
+++ Storage/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,157 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Model;
+using Storage.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Storage.ViewModel
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        List<Product> products = new List<Product>();
+        public ObservableCollection<string> ProductNames { get; set; }
+        public ObservableCollection<string> ActiveListBox { get; set; }
+        public ObservableCollection<string> PickedListBox
+        {
+            get; set;
+        }
+        public int CountValue { get; set; } = 0;
+        private ICommand sendCommand;
+        private ICommand selectionChangedCommand;
+        private MainWindow mainWodow;
+        private string? selectedProduct;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string? SelectedProduct
+        {
+            get => selectedProduct;
+            set
+            {
+                if (selectedProduct != value)
+                {
+                    selectedProduct = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public MainWindowViewModel(MainWindow window) 
+        {
+            mainWodow = window;
+            ProductNames = new ObservableCollection<string>();
+            ActiveListBox = new ObservableCollection<string>();
+            PickedListBox = new ObservableCollection<string>(); 
+            try
+            {
+                string connectionString = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build().GetConnectionString("SqlClient");
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    var sql = @"SELECT p.Id, p.Name,p.Price,p.LifeInDays, m.Id, m.Name, s.Password , s.Id, s.Name
+                                FROM [Product] p 
+                                INNER JOIN Measure m ON p.MeasureId = m.Id
+                                INNER JOIN ProductSupplier ps ON p.Id = ps.ProductId
+                                INNER JOIN Supplier s ON ps.SupplierId = s.Id";
+                    IEnumerable<Product> resultProduct = connection.Query<Product, Measure, Supplier, Product>(sql, (product, mesure, suplier) =>
+                    {
+                        product.Measure = mesure;
+                        product.Suppliers.Add(suplier);
+                        return product;
+                    });
+
+                    foreach (Product value in resultProduct)
+                    {
+                        products.Add(value);
+                        ProductNames.Add(value.Name);
+                        
+                    }
+                    MessageBox.Show(ProductNames.Count.ToString());
+                    sql = @"SELECT o.Id, o.Count, p.Name
+                            FROM [Order] o
+                            INNER JOIN Product p ON p.Id = o.ProductId";
+                    IEnumerable<Order> resultOrder = connection.Query<Order, Product, Order>(sql, (order, product) =>
+                    {
+                        order.Product = product;
+                        return order;
+                    }, splitOn: "Name");
+                    foreach (Order value in resultOrder)
+                    {
+                        ActiveListBox.Add($"{value.Id},{value.Product.Name},{value.Count}");
+                    }
+                    sql = @"SELECT s.Id, s.Count,s.Price,s.ShippingDate, p.Name
+                            FROM [Supplie] s
+                            INNER JOIN Product p ON p.Id = s.ProductId";
+                    IEnumerable<Supplies> resultSupplies = connection.Query<Supplies, Product, Supplies>(sql, (supplies, product) =>
+                    {
+                        supplies.Product = product;
+                        return supplies;
+                    }, splitOn: "Name");
+                    foreach (Supplies value in resultSupplies)
+                    {
+                        PickedListBox.Add($"{value.Id},{value.Product.Name},{value.Count},{value.Price},{value.ShippingDate.ToShortDateString()}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public ICommand? SelectionChangedCommand => selectionChangedCommand ??= new RelayCommand(param =>
+        {
+            var obj = products.Find(p => p.Name == selectedProduct);
+            mainWodow.MeasureLabel.Content = obj.Measure.Name;
+        });
+        public ICommand? SendCommand => sendCommand ??= new RelayCommand(
+            param =>
+            {
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection(new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build().GetConnectionString("SqlClient")))
+                    {
+                        connection.Open();
+                        connection.Query<Order>("INSERT [Order] VALUES (@ProductId,@StorageId, @Count,1)",
+                    new { ProductId = products.Find(p => p.Name == selectedProduct).Id, StorageId = 1, Count = mainWodow.IntegerCount.Value });
+
+
+                        var sql = @"SELECT o.Id, o.Count, p.Name
+                            FROM [Order] o
+                            INNER JOIN Product p ON p.Id = o.ProductId";
+                        IEnumerable<Order> resultOrder = connection.Query<Order, Product, Order>(sql, (order, product) =>
+                        {
+                            order.Product = product;
+                            return order;
+                        }, splitOn: "Name");
+                        ActiveListBox.Clear();
+                        foreach (Order value in resultOrder)
+                        {
+                            ActiveListBox.Add($"{value.Id},{value.Product.Name},{value.Count}");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+            });
+    }
+}
