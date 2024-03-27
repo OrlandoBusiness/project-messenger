3f9c01c9c5931ff5904f6e670a1c006a8bd368e2
Ярослав Ровнянский <jarizleifr06@gmail.com>
2024-03-27 11:25:27 +0100

    Login

---
+++ Login/ViewModel/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login.ViewModel
+{
+    internal class LoginViewModel
+    {
+        public LoginViewModel() 
+        {
+        Storage.MainWindow window = new Storage.MainWindow();
+            window.Show();
+        }
+    }
+}
