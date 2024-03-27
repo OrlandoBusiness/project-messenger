a2f605f841fd1790673c8fb7f2a3c4a7bfe1a201
Ярослав Ровнянский <jarizleifr06@gmail.com>
2024-03-27 15:18:49 +0100

    SERVER AAAAAAAA

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,49 +1,142 @@
 ﻿using System.Net.Sockets;
 using System.Net;
 using System.Text;
+using Model;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+using System.Net.Http;
+using System.Collections.ObjectModel;
 
 namespace Server
 {
     struct MyConection
     {
-        public MyConection(int id, string name, string password, CancellationTokenSource tokenSource)
+        public MyConection(int id, string name, string password,string user, CancellationTokenSource tokenSource)
         {
             this.id = id;
             this.name = name;
             this.password = password;
             this.tokenSource = tokenSource;
+            this.user = user;
         }
         public int id;
         public string name;
         public string password;
+        public string user;
         public CancellationTokenSource tokenSource;
     }
     internal class Program
     {
-        static void Main(string[] args)
-        {
-
-        }
         private List<MyConection> conections = new();
+        private List<Storage> Storages = new();
+        private List<Supplier> Suppliers = new();
+        List<Product> ProductNames = new List<Product>();//
+        List<Order> ActiveListBox = new List<Order>();
+        List<Supplie> PickedListBox = new List<Supplie>();
         TcpListener? _tcpListener;
         int nexIndex = 0;
-        int maxUsers = 2;
+        int maxUsers = 10;
 
         
-        public void Start(string ip, string port)
+        public async Task Start(string ip, string port)
         {
+            try
+            {
+                string connectionString = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build().GetConnectionString("SqlClient");
 
-            IPEndPoint ipe = new IPEndPoint(IPAddress.Parse(ip), int.Parse(port));
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    IEnumerable<Storage> StorageResult = connection.Query<Storage>("SELECT * FROM [Storage]");
+
+                    foreach (Storage value in StorageResult)
+                    {
+                        Storages.Add(value);
+                    }
+
+                    IEnumerable<Supplier> SupplierResult = connection.Query<Supplier>("SELECT * FROM [Supplier]");
+
+                    foreach (Supplier value in SupplierResult)
+                    {
+                        Suppliers.Add(value);
+                    }
+                    Console.WriteLine("We Did it");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
             try
             {
                 _tcpListener = new TcpListener(IPAddress.Parse(ip), int.Parse(port));
                 _tcpListener.Start(maxUsers + 1);
-                _ = WaitForConnectionAsync(_tcpListener);
+                await WaitForConnectionAsync(_tcpListener);
             }
             catch (Exception ex)
             {
-                //MessageBox.Show($"Error. {ex.Message}");
+                //Messagebo.Show($"Error. {ex.Message}");
             }
+        }
+        async Task Login(string[] input,TcpClient tcpClient) 
+        {
+            bool IsExist = false;
+            if (input[2].Contains("storage"))
+            {
+                IsExist = Storages.Exists(s => s.Name == input[0] && s.Password == input[1]);
+            }
+            else
+            {
+                IsExist = Suppliers.Exists(s => s.Name == input[0] && s.Password == input[1]);
+            }
+
+            if (conections.Find(m => m.name == input[0]).name == input[0])
+            {
+                var msg = Encoding.UTF8.GetBytes("User with this name already conected. Use diferent name. disconnected");
+                tcpClient.GetStream().Write(msg, 0, msg.Length);
+                tcpClient.Close();
+
+                
+            }
+            else if (maxUsers == 0)
+            {
+                var msg = Encoding.UTF8.GetBytes("Server is very busy and can't handle you. Try again later. disconnected");
+                tcpClient.GetStream().Write(msg, 0, msg.Length);
+                tcpClient.Close();
+
+                
+            }
+            else if(IsExist)
+            {
+                var msg = Encoding.UTF8.GetBytes("Conection successful. You're connected");
+                tcpClient.GetStream().Write(msg, 0, msg.Length);
+
+
+
+                var tokenSourse = new CancellationTokenSource();
+                var token = tokenSourse.Token;
+                if (input[2] == "storage")
+                {
+                    var task = Task.Factory.StartNew(() => { StorageCheckUserAsync(tcpClient, input[0]); }, token);
+                }
+                else if (input[2] == "suplier")
+                {
+                    var task = Task.Factory.StartNew(() => { SuplierCheckUserAsync(tcpClient, input[0]); }, token);
+                }
+
+
+                conections.Add(new MyConection(nexIndex, input[0], input[1], input[2], tokenSourse));
+                maxUsers -= 1;
+                nexIndex++;
+
+            }
+        }
+        async Task Register(string[] input, TcpClient tcpClient) 
+        {
+        
         }
         async Task WaitForConnectionAsync(TcpListener tcpListener)
         {
@@ -55,40 +148,14 @@ namespace Server
                     var tcpClient = await tcpListener.AcceptTcpClientAsync();
                     var buff = new byte[1024];
                     tcpClient.GetStream().Read(buff);
-                    var input = Encoding.UTF8.GetString(buff).Split(" ", 2);
-                    if (conections.Find(m => m.name == input[0]).name == input[0])
+                    var input = Encoding.UTF8.GetString(buff).Split("@", 4);
+                    if (input[3].Contains("login")) 
                     {
-                        var msg = Encoding.UTF8.GetBytes("User with this name already conected. Use diferent name. Disconnected");
-                        tcpClient.GetStream().Write(msg, 0, msg.Length);
-                        tcpClient.Close();
-                        
-                        continue;
+                    await Login(input, tcpClient);
                     }
-                    else if (maxUsers == 0)
+                    else if (input[3].Contains("register")) 
                     {
-                        var msg = Encoding.UTF8.GetBytes("Server is very busy and can't handle you. Try again later. Disconnected");
-                        tcpClient.GetStream().Write(msg, 0, msg.Length);
-                        tcpClient.Close();
-                        
-                        continue;
-                    }
-                    else
-                    {
-                        var msg = Encoding.UTF8.GetBytes("Conection successful. You're Connected");
-                        tcpClient.GetStream().Write(msg, 0, msg.Length);
-
-
-
-                        var tokenSourse = new CancellationTokenSource();
-                        var token = tokenSourse.Token;
-                        var task = Task.Factory.StartNew(() => { CheckUserAsync(tcpClient, input[0], 10); }, token);
-
-
-
-                        conections.Add(new MyConection(nexIndex, input[0], input[1], tokenSourse));
-                        maxUsers -= 1;
-                        nexIndex++;
-                        
+                        await Register(input, tcpClient);
                     }
 
                 }
@@ -102,8 +169,9 @@ namespace Server
 
 
 
-        private async Task CheckUserAsync(TcpClient tcpClient, string name, int quotes)
+        private async Task StorageCheckUserAsync(TcpClient tcpClient, string name)
         {
+            var quotes = 1;
             var client = conections.Find(m => m.name == name);
             var clientId = conections.FindIndex(m => m.name == name);
             var rnd = new Random();
@@ -115,7 +183,184 @@ namespace Server
                     var buff = new byte[1024];
                     ns.Read(buff);
                     var message = Encoding.UTF8.GetString(buff);
-                    if (message.Contains("Disconect"))
+                    if (message.Contains("Disconect") || !tcpClient.Connected)
+                    {
+                        buff = Encoding.UTF8.GetBytes(message);
+                        ns.Write(buff, 0, message.Length);
+                        tcpClient.Close();
+                        conections.Remove(client);
+                        maxUsers += 1;
+                        client.tokenSource.Cancel();
+                        break;
+                    }
+
+                    if (message.Contains("startup"))
+                    {
+                        ProductNames.Clear();
+                        ActiveListBox.Clear();
+                        PickedListBox.Clear();
+
+                        try
+                        {
+                            string connectionString = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build().GetConnectionString("SqlClient");
+
+                            using (SqlConnection connection = new SqlConnection(connectionString))
+                            {
+                                connection.Open();
+                                var sql = @"SELECT p.Id, p.Name,p.Price,p.LifeInDays, m.Id, m.Name, s.Password , s.Id, s.Name
+                                FROM [Product] p 
+                                INNER JOIN Measure m ON p.MeasureId = m.Id
+                                INNER JOIN ProductSupplier ps ON p.Id = ps.ProductId
+                                INNER JOIN Supplier s ON ps.SupplierId = s.Id";
+                                IEnumerable<Product> resultProduct = connection.Query<Product, Measure, Supplier, Product>(sql, (product, mesure, suplier) =>
+                                {
+                                    product.Measure = mesure;
+                                    product.Suppliers.Add(suplier);
+                                    return product;
+                                });
+
+                                foreach (Product value in resultProduct)
+                                {
+                                    ProductNames.Add(value);
+
+                                }
+                                sql = @"SELECT o.Id, o.Count,p.Id, p.Name,p.Price,p.LifeInDays, m.Id, m.Name
+                                FROM [Order] o
+                                INNER JOIN Product p ON p.Id = o.ProductId
+                                INNER JOIN Measure m ON p.MeasureId = m.Id";
+                                IEnumerable<Order> resultOrder = connection.Query<Order, Product, Measure,  Order >(sql, (order, product, mesure) =>
+                                {
+                                    product.Measure = mesure;
+                                    //product.Suppliers.Add(suplier);
+                                    order.Product = product;
+
+                                    return order;
+                                });
+                                foreach (Order value in resultOrder)
+                                {
+                                    ActiveListBox.Add(value);
+                                }
+                                sql = @"SELECT s.Id, s.Count,s.Price,s.ShippingDate,s.ArrivalDate,p.Id, p.Name,p.Price,p.LifeInDays, m.Id, m.Name,st.Id,st.Name,st.Password,st.MaxCapacity, sup.Id,sup.Name,sup.Password,sup.Rating
+                                FROM [Supplie] s
+                                INNER JOIN Product p ON p.Id = s.ProductId
+                                INNER JOIN Measure m ON p.MeasureId = m.Id
+                                INNER JOIN Storage st ON s.StorageId = st.Id
+                                INNER JOIN Supplier sup ON s.SupplierId = sup.Id";
+                                IEnumerable<Supplie> resultSupplies = connection.Query<Supplie, Product, Measure, Storage, Supplier, Supplie>(sql, (supplies, product, measure, storage, supplier) =>
+                                {
+                                    product.Measure = measure;
+                                    supplies.Supplier = supplier;
+                                    supplies.Storage = storage;
+                                    supplies.Product = product;
+                                    return supplies;
+                                }, splitOn: "Name");
+                                foreach (Supplie value in resultSupplies)
+                                {
+                                    PickedListBox.Add(value);
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+
+                        message = ProductNames.Count.ToString();
+                        buff = Encoding.UTF8.GetBytes(message);
+                        ns.Write(buff, 0, buff.Length);
+                        buff = new byte[1024];
+                        ns.Read(buff);
+                        foreach (Product product in ProductNames)
+                        {
+                            buff = Product.GetBytes(product);
+                            ns.Write(buff, 0, buff.Length);
+                            Console.WriteLine("Product sent");
+                            buff = new byte[1024];
+                            ns.Read(buff);
+                            message = Encoding.UTF8.GetString(buff);
+                        }
+
+                        message = ActiveListBox.Count.ToString();
+                        buff = Encoding.UTF8.GetBytes(message);
+                        ns.Write(buff, 0, buff.Length);
+                        buff = new byte[1024];
+                        ns.Read(buff);
+                        foreach (Order order in ActiveListBox)
+                        {
+                            buff = Order.GetBytes(order);
+                            ns.Write(buff, 0, buff.Length);
+                            Console.WriteLine("Order sent");
+                            buff = new byte[1024];
+                            ns.Read(buff);
+                            message = Encoding.UTF8.GetString(buff);
+                        }
+
+                        message = PickedListBox.Count.ToString();
+                        buff = new byte[1024];
+                        buff = Encoding.UTF8.GetBytes(message);
+                        ns.Write(buff, 0, buff.Length);
+                        buff = new byte[1024];
+                        ns.Read(buff);
+                        message = Encoding.UTF8.GetString(buff);
+                        foreach (Supplie supplie in PickedListBox)
+                        {
+                            buff = Supplie.GetBytes(supplie);
+                            ns.Write(buff, 0, buff.Length);
+                            Console.WriteLine("Supplie sent");
+                            buff = new byte[1024];
+                            ns.Read(buff);
+                            message = Encoding.UTF8.GetString(buff);
+                        }
+
+                    }
+
+                    if (message.Contains("send"))
+                    {
+                        try
+                        {
+                            using (SqlConnection connection = new SqlConnection(new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build().GetConnectionString("SqlClient")))
+                            {
+                                message = "qwerty";
+                                buff = new byte[1024];
+                                buff = Encoding.UTF8.GetBytes(message);
+                                ns.Write(buff, 0, buff.Length);
+                                buff = new byte[1024];
+                                ns.Read(buff);
+                                Order order = Order.GetOrder(buff);
+                                connection.Open();
+                                connection.Query<Order>("INSERT [Order] VALUES (@ProductId,@StorageId, @Count,1)",
+                            new { ProductId = order.Product.Id, StorageId = 1, Count = order.Count });
+                                Console.WriteLine("Order Inserted");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                    }
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{ex.Message}");
+                }
+            }
+        }
+        private async Task SuplierCheckUserAsync(TcpClient tcpClient, string name)
+        {
+            var quotes = 1;
+            var client = conections.Find(m => m.name == name);
+            var clientId = conections.FindIndex(m => m.name == name);
+            var rnd = new Random();
+            while (true)
+            {
+                try
+                {
+                    var ns = tcpClient.GetStream();
+                    var buff = new byte[1024];
+                    ns.Read(buff);
+                    var message = Encoding.UTF8.GetString(buff);
+                    if (message.Contains("Disconect") || !tcpClient.Connected)
                     {
                         buff = Encoding.UTF8.GetBytes(message);
                         ns.Write(buff, 0, message.Length);
@@ -136,7 +381,7 @@ namespace Server
                     }
                     else if (quotes == 0)
                     {
-                        
+
                         message = "Your limit of quotes have reached maximum";
                         var msg = Encoding.UTF8.GetBytes(message);
                         ns.Write(msg, 0, message.Length);
@@ -144,7 +389,7 @@ namespace Server
                 }
                 catch (Exception ex)
                 {
-                    //Console.WriteLine($"{ex.Message}");
+                    Console.WriteLine($"{ex.Message}");
                 }
             }
         }
---
+++ Server/StartUpClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class StartUpClass
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            _ = program.Start("26.178.12.137", "7969");
+            Console.ReadKey();
+        }
+    }
+}
